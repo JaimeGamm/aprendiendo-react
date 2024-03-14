fd1e48e2ae2f314b99faccda95651f1054ea1d0e
jaimegamm <jaimegamm@gmail.com>
2024-03-14 17:45:36 -0500

    tic tac toe

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -3,32 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS ={
+  X: 'x',
+  O: 'O'
+}
 
+const board = Array(9).fill(null)
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <main className='board'>
+    <h1>Tic tac toe</h1>
+    <section className='game'>
+      {
+        board.map((_ , index) => {
+          return (
+            <div className='cell' key={index}>
+              <span className='cell_content'>           {index}
+              </span>
+            </div>
+          )
+        })
+      }  
+    </section> 
+   </main>
   )
 }
 
