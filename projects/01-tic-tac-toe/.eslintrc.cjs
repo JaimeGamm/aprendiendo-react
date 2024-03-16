f7b25de217274026a2b30edfcc0101ba63698ba7
jaimegamm <jaimegamm@gmail.com>
2024-03-16 15:30:57 -0500

    optimizacion de sintaxis

---
+++ projects/01-tic-tac-toe/.eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  settings: {
+    react: {
+      version: '^18.2.0'
+    }
+  },
+  rules: {
+  }
+}
--- projects/01-tic-tac-toe/src/logic/storage/index.js
+++ projects/01-tic-tac-toe/src/logic/storage/index.js
@@ -1,9 +1,9 @@
-export const saveGame =({board, turn}) =>{
-    window.localStorage.setItem('board', JSON.stringify(board))
-    window.localStorage.setItem('turn', turn)
+export const saveGame = ({ board, turn }) => {
+  window.localStorage.setItem('board', JSON.stringify(board))
+  window.localStorage.setItem('turn', turn)
 }
 
 export const resetGameStorage = () => {
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
+  window.localStorage.removeItem('board')
+  window.localStorage.removeItem('turn')
 }
