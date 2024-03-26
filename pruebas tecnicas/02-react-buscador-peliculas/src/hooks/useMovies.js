cd1b26ff75d2170813f268aaab843e067f414d93
jaimegamm <jaimegamm@gmail.com>
2024-03-25 23:08:01 -0500

    version 1 de useMovies

--- pruebas tecnicas/02-react-buscador-peliculas/src/hooks/useMovies.js
+++ pruebas tecnicas/02-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,26 +1,41 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { searchMovies } from "../services/movies.js";
+import { Movies } from "../components/Movies.jsx";
 
-export function useMovies({ search }) {
+export function useMovies({ search, sort }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
 
-  const getMovies = async () => {
-    if (search === previousSearch.current) return;
+  const getMovies = useMemo(() => {
+    return async () => {
+      if (search === previousSearch.current) return;
 
-    try {
-      setLoading(true);
-      setError(null);
-      previousSearch.current = search;
-      const newMovies = await searchMovies({ search });
-      setMovies(newMovies);
-    } catch (e) {
-      setError(e.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-  return { movies, getMovies, loading };
+      try {
+        setLoading(true);
+        setError(null);
+        previousSearch.current = search;
+        const newMovies = await searchMovies({ search });
+        setMovies(newMovies);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, [search]);
+
+  //const sortedMovies = sort
+  //  ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+  //  : movies;
+
+  const sortedMovies = useMemo(() => {
+    console.log("memo");
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
+
+  return { movies: sortedMovies, getMovies, loading };
 }
