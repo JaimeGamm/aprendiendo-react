63e679545d4bb9ad9aaa56bb4babb10a42f76187
jaimegamm <jaimegamm@gmail.com>
2024-05-31 16:20:48 -0500

    carga css por componentes

--- react_19/clase-react-19/src/App.jsx
+++ react_19/clase-react-19/src/App.jsx
@@ -2,7 +2,9 @@ import { useState, version } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import {Seo} from './components/Seo.jsx'
+import {Form} from './components/Form.jsx'
 function App() {
+  const [show, setShow] = useState(false)
   return (
     <>
       <Seo title = 'Hola React 19' description = 'Hola, react 19' />
@@ -11,15 +13,12 @@ function App() {
           <h3 style={{marginBottom:0}}>Hola, react 19</h3>
           <small style={{color: "yellow", fontSize: "10px"}}>La version es {version}</small>
       </div>
-      
+      <button onClick={() => setShow(!false)}>Toggle</button>
+      {show && <Form />}
+         
     </>
   )
 }
 
-function AnotherComponent () {
-  return (
-    <Seo />
-  )
-}
 
 export default App
---
+++ react_19/clase-react-19/src/components/Form.jsx
@@ -0,0 +1,15 @@
+export function Form () {
+    return (
+        <div>
+            <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+            />
+            <form action="">
+                <input type="text" />
+            <button type='submit'>Submit</button>
+      </form>
+        </div>
+        
+    )
+}
